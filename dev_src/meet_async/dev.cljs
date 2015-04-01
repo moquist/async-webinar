5ae63e518c603cf7e512fc67431a01b627c6815c
Matt Oquist <moquist@majen.net>
2015-04-01 00:21:00 -0400

    Whitespace cleanup and use 'meet-async.dev macro.

--- dev_src/meet_async/dev.cljs
+++ dev_src/meet_async/dev.cljs
@@ -1,11 +1,10 @@
 (ns meet-async.dev
-    (:require
-     [meet-async.core]
-     [figwheel.client :as fw]))
+  (:require-macros [meet-async.dev :as dev])
+  (:require [meet-async.core]
+            [figwheel.client :as fw]))
 
-(fw/start {
-  :websocket-url "ws://localhost:3449/figwheel-ws"
+(fw/start
+ {:websocket-url (dev/websocket-url)
   :on-jsload (fn []
                (js/window.location.reload)
-               (println "Cynically reloading the page because this code is not actually figwheel-safe. See https://github.com/bhauman/lein-figwheel#writing-reloadable-code if you care.")
-               )})
+               (println "Cynically reloading the page because this code is not actually figwheel-safe. See https://github.com/bhauman/lein-figwheel#writing-reloadable-code if you care."))})
